1886ac7435ca3efb430a5835a872944cbe5ef251
LimYeonKyuu <lim000606@naver.com>
2024-07-16 21:13:31 +0900

    feat: 로그인 기능 구현 완료

--- src/main/java/com/dreamteam/ssobbi/auth/controller/AuthController.java
+++ src/main/java/com/dreamteam/ssobbi/auth/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.dreamteam.ssobbi.auth.controller;
 
 import com.dreamteam.ssobbi.auth.controller.reponse.KakaoLoginResponse;
-import com.dreamteam.ssobbi.auth.controller.request.KakaoLoginRequest;
 import com.dreamteam.ssobbi.auth.service.AuthService;
 import com.dreamteam.ssobbi.auth.service.KakaoService;
 import com.dreamteam.ssobbi.auth.util.JwtUtil;
@@ -10,27 +9,38 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService authService;
+  private final AuthService authService;
 
-    private final KakaoService kakaoService;
+  private final KakaoService kakaoService;
 
-    @Value("${custom.jwt.secret}")
-    private String SECRET_KEY;
+  @Value("${custom.jwt.secret}")
+  private String SECRET_KEY;
 
-    @Value("${custom.jwt.expire-time-ms}")
-    private long EXPIRE_TIME_MS;
+  @Value("${custom.jwt.expire-time-ms}")
+  private long EXPIRE_TIME_MS;
 
-    @PostMapping("/api/auth/kakao-login")
-    public ResponseEntity<KakaoLoginResponse> kakaoLogin(KakaoLoginRequest request, HttpServletRequest httpServletRequest) {
-        UserDto dto= authService.kakaoLogin(kakaoService.kakaoLogin(request.getKakaoToken(),httpServletRequest.getServerName()));
-    return ResponseEntity.ok(
-        KakaoLoginResponse.builder().accessToken(JwtUtil.createToken(dto.getId(),SECRET_KEY,EXPIRE_TIME_MS)).build());
+  @GetMapping("/api/auth/kakao-login")
+  public ResponseEntity<KakaoLoginResponse> kakaoLogin(
+      @RequestParam String code, HttpServletRequest httpServletRequest) {
+    UserDto dto;
+    if (httpServletRequest.getServerName().equals("localhost")) {
+        System.out.println(httpServletRequest.getServerName());
+      dto = authService.kakaoLogin(kakaoService.kakaoLogin(code, "http://localhost:3000/login/oauth/kakao"));
+    } else {
+      dto =
+          authService.kakaoLogin(kakaoService.kakaoLogin(code, httpServletRequest.getServerName()));
     }
+    return ResponseEntity.ok(
+        KakaoLoginResponse.builder()
+            .accessToken(JwtUtil.createToken(dto.getId(), SECRET_KEY, EXPIRE_TIME_MS))
+            .build());
+  }
 }
