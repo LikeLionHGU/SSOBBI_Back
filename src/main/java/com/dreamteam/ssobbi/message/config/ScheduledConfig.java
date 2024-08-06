e44dc6aa87d2f732ad0ad8e39e3348d39c2dddae
LimYeonKyuu <lim000606@naver.com>
2024-08-06 18:01:59 +0900

    fix: 알림톡 기능 주석 제거

--- src/main/java/com/dreamteam/ssobbi/message/config/ScheduledConfig.java
+++ src/main/java/com/dreamteam/ssobbi/message/config/ScheduledConfig.java
@@ -1,6 +1,5 @@
 package com.dreamteam.ssobbi.message.config;
 import com.dreamteam.ssobbi.message.service.CoolSMSService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,7 +14,7 @@ public class ScheduledConfig {
 		this.coolSMSService = coolSMSService;
 	}
 
-//	@Scheduled(cron = "0 0 21 * * *", zone = "Asia/Seoul")
+	@Scheduled(cron = "0 0 21 * * *", zone = "Asia/Seoul")
 	public void sendDailyMessage() {
 		coolSMSService.sendMessage();
 	}
